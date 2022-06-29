094a661c9dd5760a42505d91eef339911bfcc14f
Keitija Janevica <k.janevica@gmail.com>
2022-06-29 17:27:12 +0300

    training project

---
+++ JavaScript/Project Line Stays/Scripts/script.js
@@ -0,0 +1,27 @@
+function swapImage() {
+
+    
+}
+
+
+
+
+/* (d) {
+    if (!d.osrc) d.osrc=d.src; //save the original src file
+    if (d.blanked){
+      d.src = d.osrc;
+      d.blanked = null;
+    } else {
+      d.blanked = true;
+      d.src='./Photoshoot/Line_stays_by_Keitija_Janevica_steel_corset_01.jpg';
+    }
+  
+for (var i=0; i<4; i++){   //change the upper bound as you add more images
+    var obj=document.getElementById('img'+i);
+    if (obj == d) continue; //skip self
+    obj.blanked = null;
+    obj.src=obj.osrc;
+  }
+
+}
+
